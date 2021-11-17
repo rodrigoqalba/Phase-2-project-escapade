50f8ebeb780ffa108975572d3273c23d8119a177
Joshua <jdhawks@gmail.com>
2021-11-17 08:22:05 -0600

    landing page update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,16 @@
 import './Home.css'
+import SAM from '../../assets/SAMap.png'
 
 const Home = () => {
     return ( 
         <div className="landing">
             <div className="container">
                 <h1 className="page-title">Plan Your Next Escapade</h1>
+                <div className="sam">
+                    <img src={SAM} alt="south america" />
+                </div>
             </div>
+            
         </div>
      );
 }
