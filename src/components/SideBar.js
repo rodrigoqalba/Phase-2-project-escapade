cc0f443bd582c289bd2b2eee09330cef53c2a50c
Cristofer <cristoferlorenzana@gmail.com>
2021-11-17 09:13:35 -0600

    sidebar done

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,27 +1,44 @@
 import ThemeSelector from './ThemeSelector'
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import { useTheme } from '../hooks/useTheme'
 // // styles
 import './SideBar.css'
 
 
 function SideBar() {
-
   const {color} = useTheme()
+  const [country, setCountry] = useState('')
+  const history = useHistory()
+  
+  const handleClick = (e) => {
+    e.preventDefault()
+    history.push(`/search?q=${country}`)
+  }
+
+  const stateChange = (e) => {
+    setCountry(e.target.textContent)
+  }
+ 
+
 
   return (
     <div className="side-bar" style={{ background: color }}>
         <div id='theme' ><ThemeSelector/></div>
-        Brazil
-        <br/>
-        Argentina
-        <br/>
-        Peru
-        <br/>
-        Bolivia
-        <br/>
-        Colombia
-        <br/>
-        Chile
+          <div className="countries">
+            <br/>
+            <p onClick={handleClick} onMouseEnter={stateChange}>Argentina</p>
+            <br/>
+            <p onClick={handleClick} onMouseEnter={stateChange}>Bolivia</p>        
+            <br/>
+            <p onClick={handleClick} onMouseEnter={stateChange}>Brazil</p>
+            <br/>
+            <p onClick={handleClick} onMouseEnter={stateChange}>Chile</p>
+            <br/>
+            <p onClick={handleClick}onMouseEnter={stateChange}>Colombia</p>
+            <br/>
+            <p onClick={handleClick} onMouseEnter={stateChange}>Peru</p>
+          </div>
     </div>
   )
 }
